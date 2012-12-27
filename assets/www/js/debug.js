c5344f026fa84d0eb41a85bab97caa44fc4d90da
haiiro.shimeji <haiiro.shimeji@gmail.com>
2012-12-27 16:18:51 +0900

    Debug pring

---
+++ assets/www/js/debug.js
@@ -0,0 +1,13 @@
+pmap.Debug = {}
+
+pmap.Debug.View = Backbone.View.extend({
+	
+	el: "#debug",
+	
+	initialize: function() {
+		$("#map").append( ( this.$el = $("<div>").attr("id","debug") ) )
+	}
+
+} )
+
+pmap.Application.getInstance().addView( pmap.Debug.View, 100, "Debug" )
