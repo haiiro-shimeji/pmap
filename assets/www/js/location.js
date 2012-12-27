34b97b8bf44158821ecb426ff3a0ec4438fa1823
haiiro.shimeji <haiiro.shimeji@gmail.com>
2012-12-27 18:42:13 +0900

    get current direction

--- assets/www/js/location.js
+++ assets/www/js/location.js
@@ -4,18 +4,63 @@ pmap.Location.View = Backbone.View.extend({
 	
 	el: "#location",
 	
+	position: { coords: {} },
+	
 	initialize: function( data ) {
+		
+		var self = this
 		
 		var debug = data.app.findView("Debug")
 		if ( debug ) {
 			debug.$el.append( ( this.$el = $("<div>").attr("id","location") ) )
 		}
 		
-		this._getCurrentPosition()
+		if ( location.href == 'file:///android_asset/www/index.html' ) {
+			document.addEventListener("deviceready", function() { self._init() }, false);
+		} else {
+			this._init()
+		}
 		
 	},
 	
-	_getCurrentPosition: function() {
+	_init: function() {
+		
+		var self = this
+		
+		var geolocationWatchId = navigator.geolocation.watchPosition(
+			function( position ) {
+				$.map( position.coords, function( value, key ) {			
+					if ( null != value ) {
+						self.position.coords[ key ] = value
+					}
+				} )
+			},
+			$.noop,
+			{
+				enableHighAccuracy: true,
+				maximumAge: 3000, 
+				timeout: 5000
+			}
+		)
+
+		//	geolocation api does not always return heading.
+		//	But navigator.compass is not always defined (in device does not support it?).
+		if ( navigator.compass ) {
+			var compassWatchId = navigator.compass.watchHeading(
+				function( heading ) {
+					$.each( heading, function( key, value ) {
+						return self.position.coords[ key ] = value
+					} )
+				},
+				$.noop
+			)
+		}
+
+		this._debugPrintPosition()
+			
+	},
+	
+	_debugPrintPosition: function() {
 		
 		var self = this
 		
@@ -25,54 +70,18 @@ pmap.Location.View = Backbone.View.extend({
 			return $.Deferred( function(defer) {
 				setTimeout( function() {
 					defer.resolve()
-				}, 5000 )
+				}, 1000 )
 			} )
 			.promise()
 		} )
 		.done( function() {
-			//start animation.
-			self.$el.text("get location..")
+			self.$el.html( $.map( self.position.coords, function( value, key ) {
+					return key + ": " + value
+				} )
+				.join( "<br/>" ) )
 		} )
 		.pipe( function() {
-			return $.Deferred( function(defer) {
-				navigator.geolocation.getCurrentPosition(
-					function( position ) {
-						defer.resolve( position )
-					},
-					function( error ) {
-						defer.reject( error )
-					},
-					{ 
-						enableHighAccuracy: true 
-					}
-				)
-			} )
-			.promise()
-		} )
-		.always( function() {
-			//stop animation.
-		} )
-		.pipe( 
-			function( position ) {
-				self.$el.html( $.map( position.coords, function( value, key ) {
-						return key + ": " + value
-					} )
-					.join( "<br/>" ) )
-				return $.Deferred().resolve()
-			},
-			function( error ) {
-				self.$el.html( 
-					"error!<br/>"
-					+ $.map( error, function( value, key ) {
-							return key + ": " + value
-						} )
-						.join( "<br/>" ) 
-				)
-				return $.Deferred().resolve()
-			} 
-		)
-		.pipe( function() {
-			return self._getCurrentPosition()
+			return self._debugPrintPosition()
 		} )
 		
 	}
--- assets/www/js/stub.js
+++ assets/www/js/stub.js
@@ -1,11 +1,31 @@
 if (  !( location.href == 'file:///android_asset/www/index.html' ) ) {
-	navigator.geolocation = {}
-	navigator.geolocation.getCurrentPosition = function( success, error ) {
-		success( {
-			coords: {
-				latitude: 70,
-				longitude: 140
+	
+	navigator.compass = {
+		
+		watchHeading: function( success, error, options ) {
+			var _callSuccess = function( interval, _callback ) {
+				return $.Deferred()
+					.resolve()
+					.pipe( function() {
+						return $.Deferred( function(defer) {
+							setTimeout( function() { defer.resolve() }, interval )
+						} ).promise()
+					} )
+					.done( function() {
+						_callback( {
+							magneticHeading: 33
+						} )
+					} )
+					.pipe( function() {
+						return _callSuccess( interval, _callback )
+					} )
 			}
-		} )
-	} 
+
+			options = options || {}
+			_callSuccess( options.frequency || 100, success )
+			
+		}
+		
+	}
+	
 }
