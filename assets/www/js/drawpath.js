9c14e8d86350886bff23b19140484c276e01d958
haiiro.shimeji <haiiro.shimeji@gmail.com>
2013-01-31 14:26:17 +0900

    Unit test for drawpath.js

--- assets/www/js/drawpath.js
+++ assets/www/js/drawpath.js
@@ -19,60 +19,45 @@ pmap.DrawPath.View = Backbone.View.extend({
             .select()
             .toArray()
 
+        var pathBuilder = new pmap.DrawPath.PathBuilder
+
         this.$el.click(function() {
             if (!self.toggled) {
                 $.each(navigations, function(i, c) {
                     c.deactivate()
                 })
+                $("#main").append(
+                    $("<div>").slider()
+                )
                 self.toggled = true
             } else {
                 $.each(navigations, function(i, c) {
                     c.activate()
                 })
+                pathBuilder.stop()
                 self.toggled = false
             }
         })
 
-        var drawing = false
-        var points = undefined
-        var c = 0
-
-        var mapOffset = $("#map").offset()
-
         $("#map")
         .mousedown(function (e) {
             if (self.toggled) {
-                drawing = true
-                points = [{
-                    x: e.pageX - mapOffset.left,
-                    y: e.pageY - mapOffset.top
-                }]
-                c = 0
+                pathBuilder.start()
+                pathBuilder.addPoint(e)
             }
         })
         .mousemove(function (e) {
-            if (self.toggled && drawing) {
-                var v2 = {
-                        x: e.pageX - mapOffset.left,
-                        y: e.pageY - mapOffset.top
-                    },
-                    v1 = points[points.length-1]
-
-                if (3 > points.length) {
-                    points.push(v2)
-                } else {
-                    var v0 = points[points.length-2]
-                    if (0 != (v1.x-v0.x) * (v2.y-v1.y) - (v2.x-v1.x) * (v1.y-v0.y)) {
-                        points.push(v2)
-                    }
-                }
+            if (self.toggled) {
+                pathBuilder.addPoint(e)
             }
         })
-        .mouseup(function (e) {
-            if (self.toggled && drawing) {
-                pmap.Map.getInstance().addLocalPath(null, points)
-                console.log(points.length)
-                drawing = false
+        .bind("mouseup mouseleave", function (e) {
+            if (self.toggled) {
+                pathBuilder.addPoint(e)
+                if (pathBuilder.drawing) {
+                    pmap.Map.getInstance().addLocalPath(null, pathBuilder.points)
+                    pathBuilder.stop()
+                }
             }
         })
 
@@ -83,3 +68,57 @@ pmap.DrawPath.View = Backbone.View.extend({
 })
 
 pmap.Application.getInstance().addView( pmap.DrawPath.View, 101, "DrawPath" )
+
+pmap.DrawPath.checkLinearExtended = function (points, newPoint) {
+
+    if (2 > points.length) {
+        return false
+    } else {
+        var v2 = newPoint,
+            v1 = points[points.length-1]
+            v0 = points[points.length-2]
+
+        return 0 == (v1.x-v0.x) * (v2.y-v1.y) - (v2.x-v1.x) * (v1.y-v0.y)
+    }
+
+}
+
+pmap.DrawPath.PathBuilder = function () {
+    this.mapOffset = $("#map").offset()
+}
+
+pmap.DrawPath.PathBuilder.prototype = {
+
+    drawing: false,
+    points: undefined,
+
+    mapOffset: undefined,
+
+    _makePoint: function(e) {
+        return {
+            x: e.pageX - this.mapOffset.left,
+            y: e.pageY - this.mapOffset.top
+        }
+    },
+
+    start: function () {
+        this.drawing = true
+        this.points = []
+    },
+
+    stop: function () {
+        this.drawing = false
+    },
+
+    addPoint: function (e) {
+        if (this.drawing) {
+            var newPoint = this._makePoint(e)
+            if (!pmap.DrawPath.checkLinearExtended(this.points, newPoint)) {
+                this.points.push(newPoint)
+            } else {
+                this.points[this.points.length-1] = newPoint
+            }
+        }
+    }
+
+}
---
+++ qunit/js/pmap/drawpath.js
@@ -0,0 +1,89 @@
+test("pmap.DrawPath.checkLinearExtended", function () {
+
+    //  if newPoint is linear extended, returns true
+    (function () {
+        var points = [
+                { x: 1, y: 1 },
+                { x: 2, y: 2 }
+            ],
+            newPoint = { x: 3, y: 3 }
+
+        strictEqual(pmap.DrawPath.checkLinearExtended(points, newPoint), true)
+    })()
+
+    //  if not linear, returns false
+    ;(function() {
+        var points = [
+                { x: 1, y: 1 },
+                { x: 2, y: 2 }
+            ],
+            newPoint = { x: 3, y: 4 }
+
+        strictEqual(pmap.DrawPath.checkLinearExtended(points, newPoint), false)
+    })()
+
+    //  if could not decide linear or not, returns false.
+    ;(function() {
+        var points = [
+                { x: 1, y: 1 }
+            ],
+            newPoint = { x: 3, y: 3 }
+
+        strictEqual(pmap.DrawPath.checkLinearExtended(points, newPoint), false)
+    })()
+
+})
+
+test("pmap.DrawPath.PathBuilder", function () {
+
+    (function() {
+        var points = [
+                { pageX: 1, pageY: 1 },
+                { pageX: 2, pageY: 4 },
+                { pageX: 3, pageY: 8 }
+            ]
+        var instance = new pmap.DrawPath.PathBuilder
+        instance.mapOffset = { left: 0, top: 0 }
+        instance.start()
+        instance.addPoint(points[0])
+        instance.addPoint(points[1])
+        instance.addPoint(points[2])
+        instance.stop()
+
+        deepEqual(
+            instance.points,
+            [
+                { x: 1, y: 1 },
+                { x: 2, y: 4 },
+                { x: 3, y: 8 }
+            ]
+        )
+
+    })()
+
+    //  confirm lineal extended check
+    ;(function() {
+        var points = [
+                { pageX: 1, pageY: 1 },
+                { pageX: 2, pageY: 4 },
+                { pageX: 3, pageY: 7 }
+            ]
+        var instance = new pmap.DrawPath.PathBuilder
+        instance.mapOffset = { left: 0, top: 0 }
+        instance.start()
+        instance.addPoint(points[0])
+        instance.addPoint(points[1])
+        instance.addPoint(points[2])
+        instance.stop()
+
+        deepEqual(
+            instance.points,
+            [
+                { x: 1, y: 1 },
+                { x: 3, y: 7 }
+            ]
+        )
+
+    })()
+
+})
